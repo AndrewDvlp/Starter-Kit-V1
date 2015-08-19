2d4ebc3a9a100689cf0f0b90ed0f4a4fa200d10a
Andrew <andrewaguero@gmail.com>
2015-08-19 16:14:44 +0100

    fixed css linking in index.jade and updated gulpfile to replace image optimization module and updated paths

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var	gulp		= require('gulp'),
 	prefix		= require('gulp-autoprefixer'),
 	cache		= require('gulp-cache'),
 	concat		= require('gulp-concat'),
+	imageop		= require('gulp-image-optimization'),
 	jade		= require('gulp-jade'),
 	jshint		= require('gulp-jshint'),
 	jshStylish	= require('jshint-stylish'),
-	imagemin	= require('gulp-imagemin'),
 	minifycss	= require('gulp-minify-css'),
 	plumber		= require('gulp-plumber'),
 	rename		= require('gulp-rename'),
@@ -18,7 +18,7 @@ var	gulp		= require('gulp'),
 gulp.task('browser-sync', function() {
 	browserSync({
 		server: {
-			baseDir: "./build/"
+			baseDir: "build/"
 		},
 		notify: false
 	});
@@ -31,18 +31,18 @@ gulp.task('bs-reload', function() {
 
 // Optimize images   ==> Learn more about it. !!
 gulp.task('images', function() {
-	return gulp.src('./src/assets/images/**/*')
-	.pipe(cache(imagemin({
-			opmizationLevel: 3,
-			progressive: true,
-			interlaced: true
-		})))
-	.pipe(gulp.dest('build/assets/images/'));
+	return gulp.src(['src/assets/images/**/*.png','src/assets/images/**/*.jpg','src/assets/images/**/*.gif','src/assets/images/**/*.jpeg'])
+    .pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    }))
+    .pipe(gulp.dest('build/assets/images'));
 });
 
 // Process Jade files to HTML
 gulp.task('jade', function() {
-	return gulp.src('./src/jade/*.jade')
+	return gulp.src('src/jade/*.jade')
 	.pipe(plumber({
 		errorHandler: function (error) {
 			console.log(error.message);
@@ -52,7 +52,7 @@ gulp.task('jade', function() {
 	.pipe(jade({
 		'pretty': true
 	}))
-	.pipe(gulp.dest('./build/'))
+	.pipe(gulp.dest('build/'))
 	.pipe(browserSync.reload({
 		stream: true
 	}));
@@ -60,7 +60,7 @@ gulp.task('jade', function() {
 
 // Process Sass (and Scss?) to CSS
 gulp.task('styles', function() {
-	return gulp.src('./src/assets/css/**/*')
+	return gulp.src('src/assets/css/**/*')
 	.pipe(plumber({
 		errorHandler: function (error) {
 			console.log(error.message);
@@ -74,13 +74,13 @@ gulp.task('styles', function() {
 		browsers: ['last 2 versions'],
 		cascade: false
 	}))
-	.pipe(gulp.dest('./build/assets/css/'))
+	.pipe(gulp.dest('build/assets/css/'))
 	.pipe(browserSync.reload({ stream: true }));
 });
 
 // Process JavaScript files
 gulp.task('js', function() {
-	return gulp.src('./src/assets/js/**/*.js')
+	return gulp.src('src/assets/js/**/*.js')
 	.pipe(plumber({
 		errorHandler: function (error) {
 			console.log(error.message);
@@ -91,12 +91,12 @@ gulp.task('js', function() {
 	.pipe(jshint.reporter('jshint-stylish', {verbose: true}))
 	.pipe(jshint.reporter('default'))
 	.pipe(concat('main.js'))
-	.pipe(gulp.dest('./build/js/'))
+	.pipe(gulp.dest('build/js/'))
 	.pipe(rename({
 		suffix: '.min'
 	}))
 	.pipe(uglify())
-	.pipe(gulp.dest('./build/assets/js/'))
+	.pipe(gulp.dest('build/assets/js/'))
 	.pipe(browserSync.reload({
 		stream: true
 	}));
@@ -108,15 +108,15 @@ gulp.task('build', ['styles', 'jade', 'js', 'images']);
 // Watch files for changes
 gulp.task('watch', function() {
 	// Watch for styles changes and compile
-	gulp.watch(['./src/assets/css/**/*.sass', './src/assets/css/**/*.scss'], ['styles']);
+	gulp.watch(['src/assets/css/**/*.sass', 'src/assets/css/**/*.scss'], ['styles']);
 	// Watch for images and optimize them
-	gulp.watch('./src/assets/images/**/*', ['images']);
+	gulp.watch('src/assets/images/**/*', ['images']);
 	// Watch for JavaScript changes and compile
-	gulp.watch('./src/assets/js/**/*.js', ['js']);
+	gulp.watch('src/assets/js/**/*.js', ['js']);
 	// Watch for jade changes and compile
-	gulp.watch('./src/jade/**/*.jade', ['jade']);
+	gulp.watch('src/jade/**/*.jade', ['jade']);
 	// Watch for HTML files and reload browser
-	gulp.watch('./**/*.html', ['bs-reload']);
+	gulp.watch('**/*.html', ['bs-reload']);
 })
 
 // Compile, Serve and Watch
